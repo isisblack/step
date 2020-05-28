582308c084eabf89bc7f5131add19da2386b27e8
Isis Black <isismblack@gmail.com>
2020-05-28 15:05:17 -0400

    Change random greeting to random fact

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I was born in Japan!', 'Orange is my favorite color!', 'I play volleyball！', 'I am allergic to cats and dogs!'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
