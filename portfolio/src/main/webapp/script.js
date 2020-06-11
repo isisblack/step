964ceaa020211157c6faa1410ec08fe217abec1e
Isis Black <isismblack@gmail.com>
2020-06-11 14:40:59 +0000

    Fetch 'Hello Isis' from /data url

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,3 +11,34 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+/**
+ * Fetches content of /data from the server and adds it to the DOM.
+ */
+function getData() {
+  console.log('Fetching data.');
+
+  const responsePromise = fetch('/data');
+
+  responsePromise.then(handleResponse);
+}
+
+/**
+ * Handles response by converting it to text and passing the result to
+ * addDataToDom().
+ */
+function handleResponse(response) {
+  console.log('Handling the response.');
+
+  const textPromise = response.text();
+
+  textPromise.then(addDataToDom);
+}
+
+/** Adds data to the DOM. */
+function addDataToDom(data) {
+  console.log('Adding data to dom: ' + data);
+
+  const dataContainer = document.getElementById('data-container');
+  dataContainer.innerText = data;
+}
